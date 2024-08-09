5665727f1317d85add8985b1df3c1e11afdc7ea9
ohkangwook <kanguk456@gmail.com>
2024-08-09 18:50:11 +0900

    3단계 수정

--- src/main/java/com/example/todolist/Service/ScheduleServiceImpl.java
+++ src/main/java/com/example/todolist/Service/ScheduleServiceImpl.java
@@ -12,6 +12,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -21,17 +22,17 @@ public class ScheduleServiceImpl implements ScheduleService {
 
     @Override
     public Schedule createSchedule(ScheduleRequestDto scheduleRequestDto) {
-            Schedule schedule =Schedule.builder()
-                    .task(scheduleRequestDto.getTask())
-                    .writer(scheduleRequestDto.getWriter())
-                    .password(scheduleRequestDto.getPassword())
-                    .build();
-            return scheduleRepository.save(schedule);
+        Schedule schedule = Schedule.builder()
+                .task(scheduleRequestDto.getTask())
+                .writer(scheduleRequestDto.getWriter())
+                .password(scheduleRequestDto.getPassword())
+                .build();
+        return scheduleRepository.save(schedule);
     }
 
     @Override
     public ScheduleResponseDto readByScheduleId(Long sid) {
-        Schedule schedule = scheduleRepository.findBySid(sid).orElseThrow(()->new NoSuchElementException("해당 할일은 존재 하지 않습니다."));
+        Schedule schedule = scheduleRepository.findBySid(sid).orElseThrow(() -> new NoSuchElementException("해당 할일은 존재 하지 않습니다."));
         ScheduleResponseDto scheduleResponseDto = ScheduleResponseDto.builder()
                 .sid(sid)
                 .task(schedule.getTask())
@@ -43,16 +44,11 @@ public class ScheduleServiceImpl implements ScheduleService {
 
     @Override
     public List<Schedule> getSchedule(String writer, LocalDate modDate) {
-        if(writer != null && modDate != null) {
-            return scheduleRepository.findByWriterAndModDateOrderByModDateDesc(writer,modDate);
-        }
-        else if(writer != null) {
-            return scheduleRepository.findByWriterOrderByModDateDesc(writer);
-        }
-        else if(modDate != null) {
-            return scheduleRepository.findByModDateOrderByModDateDesc(modDate);
-        }
-        else
-            return scheduleRepository.findAllByOrderByModDateDesc();
+        return scheduleRepository.findAll().stream()
+                .filter(schedule -> writer == null || schedule.getWriter().equals(writer))
+                .filter(schedule -> modDate == null || schedule.getModDate().equals(modDate))
+                .sorted((s1, s2) -> s2.getModDate().compareTo(s1.getModDate()))
+                .collect(Collectors.toList());
     }
+
 }
